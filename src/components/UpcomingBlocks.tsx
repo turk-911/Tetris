c9a4211107ccd006333d10d010c29e723881f6ad
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-07-15 09:08:25 +0530

    Added upcoming blocks component

---
+++ src/components/UpcomingBlocks.tsx
@@ -0,0 +1,34 @@
+import { Block, SHAPES } from "../types";
+interface Props {
+  upcomingBlocks: Array<Block>;
+}
+export default function UpcomingBlocks({ upcomingBlocks }: Props) {
+  return (
+    <div className="upcoming">
+      {upcomingBlocks.map((block, blockIndex) => {
+        const shape = SHAPES[block].shape.filter((row) =>
+          row.some((cell) => cell)
+        );
+        return (
+          <div key={blockIndex}>
+            {shape.map((row, rowIndex) => {
+              return (
+                <div key={rowIndex} className="row">
+                  {row.map((isSet, cellIndex) => {
+                    const cellClass = isSet ? block : "hidden";
+                    return (
+                      <div
+                        key={`${blockIndex}-${rowIndex}-${cellIndex}`}
+                        className={`cell ${cellClass}`}
+                      ></div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
