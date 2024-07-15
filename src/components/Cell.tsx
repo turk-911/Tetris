e3840f45516fcf0d23576f0106d65dd0dbb00347
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-07-15 08:59:10 +0530

    Added Cell component

---
+++ src/components/Cell.tsx
@@ -0,0 +1,7 @@
+import { CellOptions } from "../types";
+interface Props {
+    type: CellOptions;
+};
+export default function Cell({ type }: Props) {
+    return <div className={`cell ${type}`}></div>
+}
