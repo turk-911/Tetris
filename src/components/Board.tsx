985ac8d349c685d5d3dd424def8f46ba2b7ffb8e
Mokshe Jain <147153672+turk-911@users.noreply.github.com>
2024-07-15 09:10:10 +0530

    Added Board component

---
+++ src/components/Board.tsx
@@ -0,0 +1,18 @@
+import Cell from "./Cell";
+import { BoardShape } from "../types";
+interface Props {
+    currentBoard: BoardShape;
+};
+export default function Board({ currentBoard }: Props) {
+    return (
+        <div className="board">
+            {currentBoard.map((row, rowIndex) => (
+                <div className="row" key={`${rowIndex}`}>
+                    {row.map((cell, colIndex) => (
+                        <Cell key={`${rowIndex}-${colIndex}`} type={cell} />
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
+}
